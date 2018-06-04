611d84bc50951ec119d63f08d661fc101c4bbe65
黄俊马 <huangjunma@promote.cache-dns.local>
2018-06-04 21:37:50 +0800

    使用网络数据

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,62 @@
 //index.js
 //获取应用实例
 
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
+const weatherColorMap = {
+  'sunny': '#cbeefd',
+  'cloudy': '#deeef6',
+  'overcast': '#c6ced2',
+  'lightrain': '#bdd5e1',
+  'heavyrain': '#c5ccd0',
+  'snow': '#aae1fc'
+}
+
 Page({
+  data: {
+    nowTemp: '11°',
+    nowWeather: '阴天',
+    nowWeatherBackground: ""
+  },
+  onPullDownRefresh(){
+    this.getNow(()=>{
+      wx.stopPullDownRefresh()
+    })
+  },
+  onLoad(){
+    this.getNow()
+  },
+  getNow(callback) {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: '广州市'
+      },
+      success: res => {
+        console.log(res)
+        let result = res.data.result
+        let temp = result.now.temp
+        let weather = result.now.weather
+        console.log(temp, weather)
+        this.setData({
+          nowTemp: temp +"°",
+          nowWeather: weatherMap[weather],
+          nowWeatherBackground: '/images/' + weather + '-bg.png'
+        }),
+          wx.setNavigationBarColor({
+            frontColor: '#000000',
+            backgroundColor: weatherColorMap[weather],
+          })
+      },
+      complete: () => {
+        callback && callback()
+      }
+    })
+  } 
 })
