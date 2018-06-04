cb4467d57aaea81ee8ea25e7aa18d4559db55c14
mike <junhuivi@163.com>
2018-06-05 06:19:59 +0800

    构建列表

--- pages/index/index.js
+++ pages/index/index.js
@@ -22,7 +22,8 @@ Page({
   data: {
     nowTemp: '11°',
     nowWeather: '阴天',
-    nowWeatherBackground: ""
+    nowWeatherBackground: "",
+    hourlyWeather: []
   },
   onPullDownRefresh(){
     this.getNow(()=>{
@@ -41,22 +42,43 @@ Page({
       success: res => {
         console.log(res)
         let result = res.data.result
-        let temp = result.now.temp
-        let weather = result.now.weather
-        console.log(temp, weather)
-        this.setData({
-          nowTemp: temp +"°",
-          nowWeather: weatherMap[weather],
-          nowWeatherBackground: '/images/' + weather + '-bg.png'
-        }),
-          wx.setNavigationBarColor({
-            frontColor: '#000000',
-            backgroundColor: weatherColorMap[weather],
-          })
+        this.setNow(result)
+        this.setHourlyWeather(result)
       },
       complete: () => {
         callback && callback()
       }
     })
-  } 
+  }, 
+setNow(result){
+    let temp = result.now.temp
+    let weather = result.now.weather
+    console.log(temp, weather)
+    this.setData({
+      nowTemp: temp + "°",
+      nowWeather: weatherMap[weather],
+      nowWeatherBackground: '/images/' + weather + '-bg.png'
+    }),
+      wx.setNavigationBarColor({
+        frontColor: '#000000',
+        backgroundColor: weatherColorMap[weather],
+      })
+  },
+
+  setHourlyWeather(result){
+    let forecast = result.forecast
+    let nowHour = new Date().getHours()
+    let hourlyWeather = []
+    for (let i = 0; i < 8; i += 1) {
+      hourlyWeather.push({
+        time: (i * 3 + nowHour) % 24 + '时',
+        iconPath: '/images/' + forecast[i].weather + '-icon.png',
+        temp: forecast[i].temp + '°'
+      })
+    }
+    hourlyWeather[0].time = '现在'
+    this.setData({ hourlyWeather: hourlyWeather })
+  }
+
+
 })
